f53f1f85acc294195839184adf0655bd338f7ef0
Rumble Roar <lnyopp15@gmail.com>
2018-07-21 09:30:26 -0400

    a few changes to logic

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,47 +1,35 @@
 //global random number variable. 
-var number = Math.floor(Math.random() * 100) + 1;
-var wins = 0
-var losses = 0  
+var number = Math.floor(Math.random() * 100) + 1; 
+var wins = 0;
+var losses = 0 ; 
+// this array is for totaling random gem guesses. 
+var gemTotal = [];
 
 
-//Gem divs & number generation
+
 $(document).ready(function() {
-
-    $("#computer-choice").text(number);
     
-    $("#random-click-1").on("click", function() {
+    // computer generated random number 
+    $("#computer-choice").text(number);
+   
+    //random number generation for gems
+
+    $("#random-click-1").on("click", function gemOne() {
 
       // ... we generate a random number
       var random = Math.floor(Math.random() * 10) + 1;
         
-    //
-    $("#random-number-1").text(random);
-    });
+    //unsure what method to use here...
 
-    $("#random-click-2").on("click", function() {
-
-        // ... we generate a random number(local)
-        var random = Math.floor(Math.random() * 10) + 1;
-          
-      //
-      $("#random-number-2").text(random);
-      });
+    //this prints directly into the "random-number-1" div.
+    // $("#random-number-1").text(random);
+    // });
     
-      $("#random-click-3").on("click", function() {
 
-        // ... we generate a random number
-        var random = Math.floor(Math.random() * 10) + 1;
-          
-      //
-      $("#random-number-3").text(random);
-      });
+    //created 4 divs (1 for each cystal)
+    // created 4 additional divs to associate random number with each crystal
     
-      $("#random-click-4").on("click", function() {
+    // unsure how to get this number to be fixed, 
 
-        // ... we generate a random number
-        var random = Math.floor(Math.random() * 10) + 1;
-          
-      //
-      $("#random-number-4").text(random);
-      });
+    
 });
